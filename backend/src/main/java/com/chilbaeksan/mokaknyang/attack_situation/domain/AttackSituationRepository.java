233c25020f88aaceb4231b8ac6f1dac91d44a7fd
박도연 <ehduszkdzkd@naver.com>
2024-04-25 14:12:57 +0900

    feat: AttackSituationRepository 생성

---
+++ backend/src/main/java/com/chilbaeksan/mokaknyang/attack_situation/domain/AttackSituationRepository.java
@@ -0,0 +1,6 @@
+package com.chilbaeksan.mokaknyang.attack_situation.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AttackSituationRepository extends JpaRepository<AttackSituation, Integer> {
+}
