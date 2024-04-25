a5f71fe35815b9ac1d3894576c46e7c23052e111
Gardener <str-pes@naver.com>
2024-04-25 13:59:42 +0900

    Feat : 공통 컴포넌트 - inputBox 추가
    1. inputBox 추가 및 제작
    2. Tailwind 글씨체 및 색깔 추가

---
+++ frontend/src/components/inputbox/constants.ts
@@ -0,0 +1,16 @@
+export const INPUT_TEXT_BOX_SIZE = {
+  small: {
+    width: '280px',
+    height: '60px',
+  },
+  medium: {
+    width: '320px',
+    height: '60px',
+  },
+  large: {
+    width: '350px',
+    height: '40px',
+  },
+} as const;
+
+export const INPUT_TYPE = ['text', 'number', 'password'] as const;
--- frontend/src/pages/landing/index.tsx
+++ frontend/src/pages/landing/index.tsx
@@ -1,7 +1,32 @@
+import InputBox from '../../components/inputbox';
+
 const Landing = () => {
   return (
-    <div>
-      여기 main 맞나요?
+    <div className='flex flex-col'>
+      <InputBox
+        name={'이것'}
+        size={'small'}
+        addStyle='m-4'
+        type={'text'}
+        placeholder={'기본 inputBox'}
+        onChange={() => {}}
+      />
+      <InputBox
+        name={'이것'}
+        size={'medium'}
+        type={'text'}
+        addStyle='m-4'
+        placeholder={'로그인'}
+        onChange={() => {}}
+      />
+      <InputBox
+        name={'이것'}
+        addStyle={'text-2xl'}
+        size={'large'}
+        type={'text'}
+        placeholder={'채팅'}
+        onChange={() => {}}
+      />
     </div>
   );
 };
