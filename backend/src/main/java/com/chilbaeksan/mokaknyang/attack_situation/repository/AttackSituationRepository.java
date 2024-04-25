a04bd157c6fe2b2bf2e81bec3f52d775a0535342
박도연 <ehduszkdzkd@naver.com>
2024-04-25 14:13:48 +0900

    refactor: AttackSituationRepository 파일 구조 변경

--- backend/src/main/java/com/chilbaeksan/mokaknyang/attack_situation/repository/AttackSituationRepository.java
+++ backend/src/main/java/com/chilbaeksan/mokaknyang/attack_situation/repository/AttackSituationRepository.java
@@ -1,5 +1,6 @@
-package com.chilbaeksan.mokaknyang.attack_situation.domain;
+package com.chilbaeksan.mokaknyang.attack_situation.repository;
 
+import com.chilbaeksan.mokaknyang.attack_situation.domain.AttackSituation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AttackSituationRepository extends JpaRepository<AttackSituation, Integer> {
