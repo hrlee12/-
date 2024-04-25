dfb73dbab72398a848bb7618857250d5e681f411
정지원 <imjjw1004@naver.com>
2024-04-25 13:34:40 +0900

    feat : hashrouter 설정

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,7 +1,8 @@
 import { Routes, Route, HashRouter } from 'react-router-dom';
 
-import Landing from '../pages/landing/index';
-import Example from '../pages/expample';
+import Landing from '@/pages/landing/index';
+import Example from '@/pages/expample';
+import Cat from '@/pages/cat';
 
 const RouterComponent = () => {
   return (
@@ -9,6 +10,7 @@ const RouterComponent = () => {
       <Routes>
         <Route path='/' element={<Landing />} />
         <Route path='/example' element={<Example />} />
+        <Route path='/cat' element={<Cat />} />
       </Routes>
     </HashRouter>
   );
