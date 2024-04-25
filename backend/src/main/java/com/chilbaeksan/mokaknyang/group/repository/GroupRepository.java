5cfa36e8185f682c6bf7eb64e01b425bb9cab7c1
박도연 <ehduszkdzkd@naver.com>
2024-04-25 14:42:24 +0900

    feat: GroupRepository 생성

--- backend/src/main/java/com/chilbaeksan/mokaknyang/group/repository/GroupRepository.java
+++ backend/src/main/java/com/chilbaeksan/mokaknyang/group/repository/GroupRepository.java
@@ -1,2 +1,10 @@
-package com.chilbaeksan.mokaknyang.group.repository;public interface GroupRepository {
+package com.chilbaeksan.mokaknyang.group.repository;
+
+import com.chilbaeksan.mokaknyang.group.domain.Group;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface GroupRepository extends JpaRepository<Group, Integer> {
+    Optional<Group> findByGroupId(Integer groupId);
 }
