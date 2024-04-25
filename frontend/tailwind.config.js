02930bf586b4724bea48a4a342504631cf84ae3c
Gardener <str-pes@naver.com>
2024-04-25 14:34:18 +0900

    Feat : BasicFrame 제작
    1. BasicFrame 제작했음.
    2. 추후 tailwind.config.js 파일을 수정해야함.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,13 +4,22 @@ export default {
   theme: {
     colors: {
       inputBoxColor: '#EAD8C6',
+      frameColor: '#CBA57F',
     },
     extend: {
       fontFamily: {
         neo: ['NeoDunggeunmo'],
         dnf: ['DNFBitBitv2'],
       },
-
+      width: {
+        boxWidth: '390px',
+      },
+      height: {
+        boxHeight: '510px',
+      },
+      borderRadius: {
+        boxRadius: '50px',
+      },
       boxShadow: {
         inputBoxShadow: 'inset -4px -4px 3px rgba(0, 0, 0, 0.25)',
       },
