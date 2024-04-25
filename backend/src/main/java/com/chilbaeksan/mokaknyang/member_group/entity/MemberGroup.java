a3de17175aa1b4097b2f986ab5d7b21ced4f1e42
박도연 <ehduszkdzkd@naver.com>
2024-04-25 13:36:57 +0900

    feat: member, group, member_group entity 생성

---
+++ backend/src/main/java/com/chilbaeksan/mokaknyang/member_group/entity/MemberGroup.java
@@ -0,0 +1,40 @@
+package com.chilbaeksan.mokaknyang.member_group.entity;
+
+import com.chilbaeksan.mokaknyang.group.entity.Group;
+import com.chilbaeksan.mokaknyang.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@IdClass(MemberGroupPK.class)
+@Entity
+@Table(name="member_group")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class MemberGroup{
+    @Id
+    @JoinColumn(name="member_id")
+    @ManyToOne
+    private Member member;
+
+    @Id
+    @JoinColumn(name="group_id")
+    @ManyToOne
+    private Group group;
+
+    @CreationTimestamp
+    @Column(name="group_created_at")
+    private LocalDateTime createdAt;
+
+    @Builder
+    public MemberGroup(Member member, Group group){
+        this.member = member;
+        this.group = group;
+    }
+}
