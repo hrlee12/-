46360f4122a360010df52f308767118d535f113d
반주현 <qkswngus@gmail.com>
2024-04-26 11:21:26 +0900

    feat : 로그인 페이지 제작 시작

---
+++ frontend/src/pages/account/LoginPage.tsx
@@ -0,0 +1,69 @@
+import Button from '@/components/button';
+import BasicFrame from '@/components/frame/basicFrame';
+import InputBox from '@/components/inputbox';
+import { useState } from 'react';
+
+const LoginPage = () => {
+  const [userid, setUserId] = useState('');
+  const [password, setPassword] = useState('');
+
+  const loginClick = () => {
+    const id: string = userid;
+    const pw: string = password;
+    console.log('아이디는 입력은 -> ' + id);
+    console.log('비밀번호 입력은 -> ' + pw);
+  };
+
+  return (
+    <div>
+      <BasicFrame>
+        <div className='flex justify-center items-center'>
+          <h2 className='text-center font-dnf w-[200px] h-[60px] text-[36px] p-2'>
+            모각냥
+          </h2>
+        </div>
+        <div>
+          {/* 입력창 */}
+          <div className='flex justify-center items-center'>
+            <InputBox
+              name={'user-id'}
+              size={'small'}
+              addStyle='m-4'
+              type={'text'}
+              placeholder={'아이디'}
+              onChange={(e) => setUserId(e.target.value)}
+            />
+          </div>
+          <div className='flex justify-center items-center'>
+            <InputBox
+              name={'user-password'}
+              size={'small'}
+              addStyle='m-4'
+              type={'text'}
+              placeholder={'비밀번호'}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          {/* 버튼창 */}
+          <div className='flex justify-center items-center'>
+            <Button
+              text={'로그인'}
+              size={'small'}
+              color={'green'}
+              onClick={loginClick}
+            />
+            <Button
+              text={'회원가입'}
+              size={'small'}
+              color={'green'}
+              onClick={loginClick}
+            />
+          </div>
+        </div>
+      </BasicFrame>
+    </div>
+  );
+};
+
+export default LoginPage;
--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -3,6 +3,7 @@ import { Routes, Route, HashRouter } from 'react-router-dom';
 import Landing from '@/pages/landing/index';
 import Example from '@/pages/expample';
 import Cat from '@/pages/cat';
+import LoginPage from '@/pages/account/LoginPage';
 
 const RouterComponent = () => {
   return (
@@ -11,6 +12,7 @@ const RouterComponent = () => {
         <Route path={'/'} element={<Landing />} />
         <Route path={'/example'} element={<Example />} />
         <Route path={'/cat'} element={<Cat />} />
+        <Route path={'/login'} element={<LoginPage />} />
       </Routes>
     </HashRouter>
   );
