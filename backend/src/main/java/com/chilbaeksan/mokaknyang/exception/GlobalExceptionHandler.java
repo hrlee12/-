b34301909b7d0cfd54b3af7f1cc552455d7fcbc0
백승윤 <fangdol888@naver.com>
2024-04-26 10:48:22 +0900

    fix : 서버가 만든 에러가 아닌 내부 로직 에러 등의 나머지 에러등은 전부 서버 에러로 출력하도록 코드 추가

--- backend/src/main/java/com/chilbaeksan/mokaknyang/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/chilbaeksan/mokaknyang/exception/GlobalExceptionHandler.java
@@ -5,11 +5,23 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.lang.reflect.Executable;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    // 서버가 내보내는 에러는 아래에서 처리
     @ExceptionHandler(BaseException.class)
     public ResponseEntity<ErrorResponse> handleBaseException(BaseException ex){
         return ResponseEntity.status(ex.getErrorCode().getResultCode())
                 .body(new ErrorResponse(ex.getErrorCode().getResultCode(), ex.getErrorCode().getMessage()));
     }
+
+    // 나머지는 전부 서버 에러로 처리
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleBaseException(Exception ex){
+        ErrorResponse errorResponse = new ErrorResponse(ErrorCode.INTERNAL_SERVER_ERROR.getResultCode(), ErrorCode.INTERNAL_SERVER_ERROR.getMessage());
+        return ResponseEntity.status(errorResponse.getResultCode())
+                .body(errorResponse);
+    }
+
 }
