24bb44d23de15f348db9a0ce5b54b0ef9a68f866
반주현 <qkswngus@gmail.com>
2024-04-27 23:18:52 +0900

    feat : 비밀번호 확인 기능 추가

--- frontend/src/pages/account/SignUpPage.tsx
+++ frontend/src/pages/account/SignUpPage.tsx
@@ -1,11 +1,29 @@
 import BasicFrame from '@/components/frame/basicFrame';
 import InputBox from '@/components/inputbox';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const SignUpPage = () => {
   const [userid, setUserId] = useState('');
   const [password, setPassword] = useState('');
   const [password2, setPassword2] = useState('');
+  const [passwordMatch, setPasswordMatch] = useState(true);
+
+  const navigate = useNavigate();
+
+  const isPasswordValid = (password: string): boolean => {
+    const regex = /^(?=.*[A-Z])(?=.*[!@#$%^&*]).{15,}$/;
+
+    return regex.test(password);
+  };
+
+  useEffect(() => {
+    checkPasswordMatch();
+  }, [password, password2]);
+
+  const checkPasswordMatch = () => {
+    setPasswordMatch(password === password2);
+  };
 
   return (
     <div>
@@ -33,7 +51,10 @@ const SignUpPage = () => {
             addStyle='m-2 inputBox-font-medium'
             type={'password'}
             placeholder={'비밀번호'}
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={(e) => {
+              setPassword(e.target.value);
+              checkPasswordMatch();
+            }}
           />
         </div>
         <div className='flex justify-center items-center'>
@@ -43,10 +64,28 @@ const SignUpPage = () => {
             addStyle='m-2 inputBox-font-medium'
             type={'password'}
             placeholder={'비밀번호 확인'}
-            onChange={(e) => setPassword2(e.target.value)}
+            onChange={(e) => {
+              setPassword2(e.target.value);
+              checkPasswordMatch();
+            }}
           />
         </div>
-        <div className=''>back</div>
+        {/* 비밀번호 일치 여부 메시지 */}
+        <div className='flex justify-center items-center'>
+          {password && password2 && !passwordMatch && (
+            <p className='font-neo' style={{ color: 'rgb(239 68 68)' }}>
+              비밀번호가 일치하지 않습니다.
+            </p>
+          )}
+          {password && password2 && passwordMatch && (
+            <p className='font-neo' style={{ color: 'rgb(21 128 61)' }}>
+              비밀번호가 일치합니다.
+            </p>
+          )}
+        </div>
+        <div onClick={() => navigate('/login')} className=''>
+          back
+        </div>
       </BasicFrame>
     </div>
   );
