20059a3f806d9070921dc5b60ac71ca817d88c2a
백승윤 <fangdol888@naver.com>
2024-04-29 15:34:10 +0900

    feat : 맴버 관련 api 완성

--- backend/src/main/java/com/chilbaeksan/mokaknyang/member/domain/Member.java
+++ backend/src/main/java/com/chilbaeksan/mokaknyang/member/domain/Member.java
@@ -16,6 +16,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
+@Setter
 @Builder
 public class Member {
     @Id
---
+++ backend/src/main/java/com/chilbaeksan/mokaknyang/member/dto/MemberRegisterRequestDto.java
@@ -0,0 +1,14 @@
+package com.chilbaeksan.mokaknyang.member.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class MemberRegisterRequestDto {
+    private String memberCatName;
+    private String memberGoal;
+    private String memberCatColor;
+}
