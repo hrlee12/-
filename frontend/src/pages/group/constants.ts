92957cf5c1e678547f11d181802ce376ffd3a4de
Gardener <str-pes@naver.com>
2024-04-29 14:11:41 +0900

    Feat : Group Invite Page Design
    1. Group Invite Page 제작
    2. 이에 따른 일부 디자인, inputBox setting 추가

--- frontend/src/pages/group/constants.ts
+++ frontend/src/pages/group/constants.ts
@@ -7,4 +7,9 @@ export const GROUP_SETTING = {
   CHANGE_MASTER: '방장냥 옮기기',
   GROUP_NAME: '그룹 이름',
   GROUP_GOAL: '그룹 목표',
+  GROUP_MEMBER: '그룹 멤버',
 };
+
+export const MAKE_GROUP = '그룹 초대';
+
+export const GROUP_INVITE_MESSAGE = '친구를 그룹에 초대하시겠습니까?';
--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -5,6 +5,7 @@ import Example from '@/pages/expample';
 import Cat from '@/pages/cat';
 import GroupPage from '@/pages/group';
 import GroupSetting from '@/pages/group/setting';
+import MakeGroupPage from '@/pages/group/makeGroup';
 
 const RouterComponent = () => {
   return (
@@ -15,6 +16,7 @@ const RouterComponent = () => {
         <Route path={'/cat'} element={<Cat />} />
         <Route path={'/group'} element={<GroupPage />} />
         <Route path={'/groupSetting'} element={<GroupSetting />} />
+        <Route path={'/makeGroup'} element={<MakeGroupPage />} />
       </Routes>
     </HashRouter>
   );
