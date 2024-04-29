c4de7dc9a6e68565f977b2af6bf9b74493cf2a4f
박도연 <ehduszkdzkd@naver.com>
2024-04-29 10:01:56 +0900

    refactor: partyManger 함께 저장

--- backend/src/main/java/com/chilbaeksan/mokaknyang/party/service/PartyService.java
+++ backend/src/main/java/com/chilbaeksan/mokaknyang/party/service/PartyService.java
@@ -1,5 +1,7 @@
 package com.chilbaeksan.mokaknyang.party.service;
 
+import com.chilbaeksan.mokaknyang.member.domain.Member;
+import com.chilbaeksan.mokaknyang.member.repository.MemberRepository;
 import com.chilbaeksan.mokaknyang.party.domain.Party;
 import com.chilbaeksan.mokaknyang.party.dto.request.PartyRegist;
 import com.chilbaeksan.mokaknyang.party.repository.PartyRepository;
@@ -14,13 +16,18 @@ import org.springframework.stereotype.Service;
 @Transactional
 public class PartyService {
     private final PartyRepository partyRepository;
+    private final MemberRepository memberRepository;
 
     public Party registParty(PartyRegist partyRegist){
+        Member partyManager = memberRepository.findByMemberId(partyRegist.getPartyManagerId())
+                .orElseThrow(() -> new NullPointerException("찾으려는 member가 존재하지 않습니다."));
+
         Party party = Party.builder()
                 .name(partyRegist.getPartyName())
                 .inviteMessage(partyRegist.getPartyInviteMessage())
                 .maxNumber(partyRegist.getPartyMaxNumber())
                 .participateNumber(partyRegist.getPartyParticipateNumber())
+                .member(partyManager)
                 .build();
 
         return partyRepository.save(party);
