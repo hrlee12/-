a4684a154759a40116214a8fc7cf9120a699ed4c
박도연 <ehduszkdzkd@naver.com>
2024-04-29 15:14:53 +0900

    feat: InvitationRepository findByMemberAndParty 메서드 추가

--- backend/src/main/java/com/chilbaeksan/mokaknyang/Invitation/repository/InvitationRepository.java
+++ backend/src/main/java/com/chilbaeksan/mokaknyang/Invitation/repository/InvitationRepository.java
@@ -6,8 +6,10 @@ import com.chilbaeksan.mokaknyang.party.domain.Party;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface InvitationRepository extends JpaRepository<Invitation, Integer> {
     List<Invitation> findByMember(Member member);
     List<Invitation> findByParty(Party party);
+    Optional<Invitation> findByMemberAndParty(Member member, Party party);
 }
