1c4b534c8ba25ba2b5b5e76bef864decd2f5c48e
박도연 <ehduszkdzkd@naver.com>
2024-04-29 15:22:16 +0900

    feat: MemberParty Repository 생성

---
+++ backend/src/main/java/com/chilbaeksan/mokaknyang/member_party/repository/MemberPartyRepository.java
@@ -0,0 +1,7 @@
+package com.chilbaeksan.mokaknyang.member_party.repository;
+
+import com.chilbaeksan.mokaknyang.member_party.domain.MemberParty;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberPartyRepository extends JpaRepository<MemberParty, Integer> {
+}
