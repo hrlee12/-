19fc3937384c50e87da69f72b5c6b92c1b9307a0
Gardener <str-pes@naver.com>
2024-04-29 12:17:52 +0900

    Feat : Group Setting Page Design
    1. Group Setting Page 제작
    2. InputBox에 Setting Size 추가

---
+++ frontend/src/pages/group/setting/index.tsx
@@ -0,0 +1,77 @@
+import * as constants from '@/pages/group/constants.ts';
+import BasicFrame from '@/components/frame/basicFrame';
+import InputBox from '@/components/inputbox';
+import Button from '@/components/button';
+
+const GroupSetting = () => {
+  return (
+    <BasicFrame>
+      <div className='pt-8 pl-9'>
+        <div className='font-dnf text-4xl pb-8'>
+          {constants.GROUP_SETTING.TITLE}
+        </div>
+      </div>
+      <div>
+        <div className='font-dnf text-2xl pl-9'>
+          {constants.GROUP_SETTING.CHANGE_MASTER}
+        </div>
+        <div className={'pl-9'}>
+          <select className={'w-60 h-10 rounded bg-groupColor'}></select>
+          <Button
+            text={'저장'}
+            size={'small'}
+            color={'blue'}
+            onClick={() => {}}
+          />
+        </div>
+      </div>
+      <div className='font-dnf text-2xl pl-9 pt-1'>
+        {constants.GROUP_SETTING.GROUP_NAME}
+      </div>
+      <div className={'pl-9'}>
+        <InputBox
+          name={'그룹 이름'}
+          size={'setting'}
+          addStyle={'pb-4'}
+          type={'text'}
+          placeholder={''}
+          onChange={() => {}}
+        />
+        <Button
+          text={'저장'}
+          size={'small'}
+          color={'blue'}
+          onClick={() => {}}
+        />
+      </div>
+      <div className={'pl-9 pt-3'}>
+        <div className='font-dnf text-2xl'>
+          {constants.GROUP_SETTING.GROUP_GOAL}
+        </div>
+        <InputBox
+          name={'그룹 목표'}
+          addStyle={'pb-4'}
+          size={'setting'}
+          type={'text'}
+          placeholder={''}
+          onChange={() => {}}
+        />
+        <Button
+          text={'저장'}
+          size={'small'}
+          color={'blue'}
+          onClick={() => {}}
+        />
+      </div>
+      <Button
+        text={'그룹 삭제'}
+        addStyle={`fixed left-[275px] top-5`}
+        size={'small'}
+        color={'gray'}
+        onClick={() => {}}
+      />
+    </BasicFrame>
+  );
+};
+
+export default GroupSetting;
--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -4,6 +4,7 @@ import Landing from '@/pages/landing/index';
 import Example from '@/pages/expample';
 import Cat from '@/pages/cat';
 import GroupPage from '@/pages/group';
+import GroupSetting from '@/pages/group/setting';
 
 const RouterComponent = () => {
   return (
@@ -13,6 +14,7 @@ const RouterComponent = () => {
         <Route path={'/example'} element={<Example />} />
         <Route path={'/cat'} element={<Cat />} />
         <Route path={'/group'} element={<GroupPage />} />
+        <Route path={'/groupSetting'} element={<GroupSetting />} />
       </Routes>
     </HashRouter>
   );
