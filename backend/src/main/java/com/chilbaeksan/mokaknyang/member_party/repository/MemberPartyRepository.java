9088e5be6b38bcd04c79f2ee4194f811a84559d2
박도연 <ehduszkdzkd@naver.com>
2024-04-29 16:42:55 +0900

    feat: MemberPartyRepository findByParty 메서드 추가

--- backend/src/main/java/com/chilbaeksan/mokaknyang/member_party/repository/MemberPartyRepository.java
+++ backend/src/main/java/com/chilbaeksan/mokaknyang/member_party/repository/MemberPartyRepository.java
@@ -5,8 +5,10 @@ import com.chilbaeksan.mokaknyang.member_party.domain.MemberParty;
 import com.chilbaeksan.mokaknyang.party.domain.Party;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberPartyRepository extends JpaRepository<MemberParty, Integer> {
     Optional<MemberParty> findByMemberAndParty(Member member, Party party);
+    List<MemberParty> findByParty(Party party);
 }
