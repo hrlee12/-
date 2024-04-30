d052bdf569b8efedda6bea48cbd6ec6169209492
반주현 <qkswngus@gmail.com>
2024-04-30 13:15:40 +0900

    feat : Add hover at signup page

--- frontend/src/pages/account/SignUpPage.tsx
+++ frontend/src/pages/account/SignUpPage.tsx
@@ -3,6 +3,7 @@ import InputBox from '@/components/inputbox';
 import { useEffect, useState, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Button from '@/components/button';
+import InfoHover from './info/InfoHover';
 
 const SignUpPage = () => {
   const [userid, setUserId] = useState('');
@@ -14,10 +15,22 @@ const SignUpPage = () => {
   const navigate = useNavigate();
 
   // 기타 조건 추가 등 수정 예정
-  const signUp = (userid: string, password: string) => {
-    if (isPasswordValid(password)) {
+  const signUp = (userid: string, usernickname: string, password: string) => {
+    if (
+      userid &&
+      usernickname &&
+      password &&
+      isPasswordValid(password) &&
+      passwordMatch
+    ) {
       console.log(
-        'id = ' + userid + '  pw = ' + password + '\n 정상, API 연결 예정',
+        'id = ' +
+          userid +
+          'nickname = ' +
+          usernickname +
+          '  pw = ' +
+          password +
+          '\n 정상, API 연결 예정',
       );
     } else if (!isPasswordValid(password)) {
       console.log('비밀번호 조건을 다시 확인해주세요');
@@ -45,6 +58,7 @@ const SignUpPage = () => {
           <h2 className='text-center font-dnf w-[200px] h-[60px] text-[36px] p-2 pt-[40px]'>
             회원가입
           </h2>
+          <InfoHover />
         </div>
         {/* 입력창 */}
         <div className='flex justify-center items-center pt-[60px]'>
@@ -105,7 +119,7 @@ const SignUpPage = () => {
             text={'가입하기'}
             size={'small'}
             color={'green'}
-            onClick={() => signUp(userid, password)}
+            onClick={() => signUp(userid, usernickname, password)}
           />
         </div>
       </BasicFrame>
