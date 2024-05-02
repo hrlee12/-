5473cc73d6a607b794a747f31a48870997dc0f5c
이효리 <gyfl5542@naver.com>
2024-05-02 17:26:01 +0900

    fix: cors-config

--- backend/src/main/java/com/chilbaeksan/mokaknyang/config/WebConfig.java
+++ backend/src/main/java/com/chilbaeksan/mokaknyang/config/WebConfig.java
@@ -9,11 +9,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowedOrigins("https://mogaknyan.duckdns.org");
+                .allowedOrigins("http://localhost:5173","https://mogaknyan.duckdns.org") // 모든 출처 허용
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH", "TRACE") // 모든 HTTP 메소드 허용
+                .allowedHeaders("Content-Type", "Authorization") // 모든 헤더 허용
+                .allowCredentials(true) // 쿠키를 포함시키기 위해 필요
+                .maxAge(3600); // 사전 요청(pre-flight request) 캐싱 시간
     }
 }
 
