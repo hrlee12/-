cc0e866e213570deed4754273c14f743f91f8d7f
이효리 <gyfl5542@naver.com>
2024-05-02 13:09:56 +0900

    connect-test

---
+++ backend/src/main/java/com/chilbaeksan/mokaknyang/ConnectionTestController.java
@@ -0,0 +1,16 @@
+package com.chilbaeksan.mokaknyang;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class ConnectTestController {
+    @GetMapping("/test")
+    public ResponseEntity ConnectTest(){
+//        log.info("연결 확인");
+        return null;
+    }
+}
