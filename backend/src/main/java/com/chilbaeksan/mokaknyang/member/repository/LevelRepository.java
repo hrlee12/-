0bf81da744809ad11ddd4463f51ba8d59ca184bc
백승윤 <fangdol888@naver.com>
2024-05-03 13:48:00 +0900

    fix : 키 타입 수정

--- backend/src/main/java/com/chilbaeksan/mokaknyang/member/repository/LevelRepository.java
+++ backend/src/main/java/com/chilbaeksan/mokaknyang/member/repository/LevelRepository.java
@@ -1,8 +1,9 @@
 package com.chilbaeksan.mokaknyang.member.repository;
 
+import com.chilbaeksan.mokaknyang.member.domain.Level;
 import com.chilbaeksan.mokaknyang.member.domain.Title;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TitleRepository extends JpaRepository<Title, Short> {
+public interface LevelRepository extends JpaRepository<Level, Short> {
 
 }
