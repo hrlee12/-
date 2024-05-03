8a81bd0034c36b40924518d5edd23b3118354caf
Gardener <str-pes@naver.com>
2024-05-03 17:50:39 +0900

    Merge : 원격 작업 위해 Commit

---
+++ frontend/src/types/member.d.ts
@@ -0,0 +1,11 @@
+export interface MyInfoProps {
+  memberExp: number;
+  memberCreatedAt: string;
+  memberCatName: string;
+  memberHitNumber: number;
+  memberBehitNumber: number;
+  level: number;
+  memberGoal: string;
+  titleContent: string;
+  catAssetUrl: string;
+}
