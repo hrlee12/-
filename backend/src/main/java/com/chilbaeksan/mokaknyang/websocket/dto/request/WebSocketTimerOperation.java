cd7b8503acb3ccc8f1334d37b8b2e3b5134f2d01
박도연 <ehduszkdzkd@naver.com>
2024-05-05 16:02:55 +0900

    feat: WebSocketTimerOperation request dto 생성

---
+++ backend/src/main/java/com/chilbaeksan/mokaknyang/websocket/dto/request/WebSocketTimerOperation.java
@@ -0,0 +1,17 @@
+package com.chilbaeksan.mokaknyang.websocket.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class WebSocketTimerOperation {
+    private String memberLoginId;
+    private LocalDateTime serverTime;
+}
