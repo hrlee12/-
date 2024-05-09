deecd35569df9f73abd1766a167703255de7326b
반주현 <qkswngus@gmail.com>
2024-05-09 16:40:09 +0900

    feat : 버튼 추가 및 분기 설정
    1. 혼자 하는 뽀모도로 타이머 설정 페이지에 뒤로가기 및 화면 끄는 버튼 추가
    2. 랜딩 페이지에서 고양이 클릭할 때 로그인 여부에 따라 이동하는 페이지 분기

--- frontend/src/pages/cat/Setting/index.tsx
+++ frontend/src/pages/cat/Setting/index.tsx
@@ -6,6 +6,7 @@ import Pomodoro from '@/components/timer/pomodoro';
 import { useNavigate } from 'react-router-dom';
 import { useSkinStore } from '@/stores/useSkinStore.ts';
 import { logOut } from '@/apis/user';
+import { FaCircleChevronLeft, FaCircleXmark } from 'react-icons/fa6';
 
 const MyCatSetting = () => {
   const navigate = useNavigate();
@@ -19,40 +20,52 @@ const MyCatSetting = () => {
   };
 
   return (
-    <MyFrame>
-      <div className={'pt-10'}>
-        <div className={'flex flex-row justify-center'}>
-          <div
-            className='character-idle'
-            id='clickable-area'
-            style={{
-              backgroundImage: `url(${import.meta.env.VITE_IMG_URL}/cat_idle_0${mySkin}.png)`,
-            }}
-            onClick={() => navigate('/catSkin')}
-          ></div>
+    <>
+      <FaCircleChevronLeft
+        className='absolute ml-[40px] mt-[25px] text-[35px] text-frameColor bg-inputBoxColor rounded-boxRadius'
+        onClick={() => {
+          navigate(-1); // '/catSetting' 경로가 왜인지 모르겠지만 작동하지 않음.
+        }}
+      />
+      <FaCircleXmark
+        className='absolute ml-[455px] mt-[25px] text-[35px] text-frameColor bg-inputBoxColor rounded-boxRadius'
+        onClick={() => navigate('/')}
+      />
+      <MyFrame>
+        <div className={'pt-10'}>
+          <div className={'flex flex-row justify-center'}>
+            <div
+              className='character-idle'
+              id='clickable-area'
+              style={{
+                backgroundImage: `url(${import.meta.env.VITE_IMG_URL}/cat_idle_0${mySkin}.png)`,
+              }}
+              onClick={() => navigate('/catSkin')}
+            ></div>
+          </div>
         </div>
-      </div>
-      <div className='flex flex-col font-dnf text-2xl pl-20'>
-        {constants.POMODORO}
-      </div>
-      <Pomodoro />
-      <div className={'flex justify-center pt-5'}>
-        <Button
-          text={'냥 정보'}
-          size={'small'}
-          color={'green'}
-          onClick={() => navigate('/catInfo')}
-        />
-        <Button
-          text={'로그아웃'}
-          size={'small'}
-          color={'gray'}
-          onClick={() => {
-            logOutClick();
-          }}
-        />
-      </div>
-    </MyFrame>
+        <div className='flex flex-col font-dnf text-2xl pl-20'>
+          {constants.POMODORO}
+        </div>
+        <Pomodoro />
+        <div className={'flex justify-center pt-5'}>
+          <Button
+            text={'냥 정보'}
+            size={'small'}
+            color={'green'}
+            onClick={() => navigate('/catInfo')}
+          />
+          <Button
+            text={'로그아웃'}
+            size={'small'}
+            color={'gray'}
+            onClick={() => {
+              logOutClick();
+            }}
+          />
+        </div>
+      </MyFrame>
+    </>
   );
 };
 
