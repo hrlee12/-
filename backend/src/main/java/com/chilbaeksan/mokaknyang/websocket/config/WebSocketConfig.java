252416c2134b8c8a58f121b7ef46404a7e27029d
박도연 <ehduszkdzkd@naver.com>
2024-05-09 17:46:41 +0900

    feat: sockJS() 추가

--- backend/src/main/java/com/chilbaeksan/mokaknyang/websocket/config/WebSocketConfig.java
+++ backend/src/main/java/com/chilbaeksan/mokaknyang/websocket/config/WebSocketConfig.java
@@ -15,6 +15,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry){
         registry.addHandler(webSocketHandler, "/status")
-                .setAllowedOrigins("*");
+                .setAllowedOrigins("*")
+                .withSockJS();
     }
 }
