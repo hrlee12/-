28895e7ede48adc4376af37c6c68cb590e20799f
박도연 <ehduszkdzkd@naver.com>
2024-05-10 10:54:49 +0900

    feat: findByMember에 Pageable parameter 추가

--- backend/src/main/java/com/chilbaeksan/mokaknyang/member_party/repository/MemberPartyRepository.java
+++ backend/src/main/java/com/chilbaeksan/mokaknyang/member_party/repository/MemberPartyRepository.java
@@ -3,6 +3,7 @@ package com.chilbaeksan.mokaknyang.member_party.repository;
 import com.chilbaeksan.mokaknyang.member.domain.Member;
 import com.chilbaeksan.mokaknyang.member_party.domain.MemberParty;
 import com.chilbaeksan.mokaknyang.party.domain.Party;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -11,5 +12,5 @@ import java.util.Optional;
 public interface MemberPartyRepository extends JpaRepository<MemberParty, Integer> {
     Optional<MemberParty> findByMemberAndParty(Member member, Party party);
     List<MemberParty> findByParty(Party party);
-    List<MemberParty> findByMember(Member member);
+    List<MemberParty> findByMember(Member member, Pageable pageable);
 }
