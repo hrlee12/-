72d23c445c8b91ecfd35191111d988453aa146fb
Gardener-Soul <str-pes@naver.com>
2024-05-17 14:43:45 +0900

    Feat : 웹소켓을 바탕으로 한 고양이 여러마리 띄우기
    1. 웹소켓에 따라 고양이를 여러마리 표시 가능

--- frontend/src/pages/group/info/index.tsx
+++ frontend/src/pages/group/info/index.tsx
@@ -9,10 +9,17 @@ import ProfileCat from '@/components/cat/profile';
 import MultiPomodoro from '@/components/timer/pomodoro/multiPomodoro';
 import Button from '@/components/button';
 import { GroupSocket } from '@/apis/websocket/groupSocket.ts';
+import { getCatId } from '@/apis/member.ts';
+import IdleCat from '@/components/cat/idle';
+import { useCatStore } from '@/stores/useGroupCatStore.ts'; // 전역 상태 임포트
 
 interface StatusMessage {
   userId: number;
   status: string;
+}
+
+interface MemberId {
+  memberId: number;
 }
 
 const GroupInfoPage = () => {
@@ -30,7 +37,9 @@ const GroupInfoPage = () => {
   const partyId = Number(groupId);
   const myId = useAuthStore.getState().accessToken; // 변경: memberId를 가져옴
   const [groupInfo, setGroupInfo] = useState<GroupProps>(defaultGroupInfo);
-  const [, setUserIds] = useState<number[]>([]); // userIds 배열을 상태로 추가
+  const [userIds, setUserIds] = useState<MemberId[]>([]); // MemberId 배열을 상태로 추가
+  const setCatIdList = useCatStore((state) => state.setCatIdList); // 전역 상태 업데이트 함수 가져오기
+  const catIdList = useCatStore((state) => state.catIdList); // 전역 상태 가져오기
   const socketRef = useRef<GroupSocket | null>(null);
 
   useEffect(() => {
@@ -50,7 +59,7 @@ const GroupInfoPage = () => {
 
     socket.onMessage((data: StatusMessage[]) => {
       console.log('Received status message:', data); // 수신된 메시지를 콘솔에 출력
-      const ids = data.map((msg) => msg.userId); // userId 값을 추출하여 배열에 저장
+      const ids = data.map((msg) => ({ memberId: msg.userId })); // userId 값을 MemberId 형식으로 변환하여 배열에 저장
       setUserIds(ids); // 상태로 userId 배열을 설정
     });
 
@@ -60,6 +69,22 @@ const GroupInfoPage = () => {
       socketRef.current?.disconnect();
     };
   }, [partyId, myId]);
+
+  useEffect(() => {
+    const fetchCatIds = async () => {
+      try {
+        const response = await getCatId(userIds);
+        setCatIdList(response.catIdList); // 전역 상태로 설정
+        console.log(catIdList);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    if (userIds.length > 0) {
+      fetchCatIds();
+    }
+  }, [userIds]);
 
   const clickLeaveGroup = async (partyId: number) => {
     try {
@@ -71,69 +96,80 @@ const GroupInfoPage = () => {
   };
 
   return (
-    <BasicFrame>
-      <div className='flex flex-col'>
-        <div className='flex flex-row place-content-around gap-10'>
-          <div className='font-dnf text-4xl pl-3 pt-8'>
-            {groupInfo.partyName}
-          </div>
-          <Button
-            text={'채팅'}
-            size={'small'}
-            color={'blue'}
-            addStyle={'top-5'}
-            onClick={() => navigate(`/chatting/${partyId}`)}
-          />
-        </div>
-        <div className='font-dnf text-2xl pl-5 pt-3'>
-          {constants.GROUP_INFO.GOAL} :{' '}
-          {groupInfo.partyGoal || constants.GROUP_INFO.NO_GOAL}
-        </div>
-        <div className='font-dnf text-2xl pl-5 pt-1'>
-          {constants.GROUP_INFO.COUNT} : {groupInfo.partyMembers?.length || 0}
-        </div>
-        {groupInfo.partyMembers?.length > 0 && (
-          <div className='grid grid-cols-2'>
-            {groupInfo.partyMembers.map((member) => (
-              <div key={member.memberId} className='flex pl-5 items-center'>
-                <ProfileCat catId={member.catId} />
-                <div className='font-neo text-xl pl-2'>
-                  {member.memberCatName}
-                </div>
-              </div>
-            ))}
-          </div>
-        )}
-
-        <div className='flex flex-col font-dnf text-2xl pl-5 py-2'>
-          {constants.GROUP_INFO.POMODORO}
-        </div>
-        <MultiPomodoro groupId={partyId} />
-        <div className='flex justify-center pt-3'>
-          {myId === groupInfo.partyManagerId ? (
+    <>
+      <BasicFrame>
+        <div className='flex flex-col'>
+          <div className='flex flex-row place-content-around gap-10'>
+            <div className='font-dnf text-4xl pl-3 pt-8'>
+              {groupInfo.partyName}
+            </div>
             <Button
-              text={'그룹 설정'}
-              size='small'
-              color='navy'
-              onClick={() => navigate(`/groupSetting/${partyId}`)}
+              text={'채팅'}
+              size={'small'}
+              color={'blue'}
+              addStyle={'top-5'}
+              onClick={() => navigate(`/chatting/${partyId}`)}
             />
-          ) : (
+          </div>
+          <div className='font-dnf text-2xl pl-5 pt-3'>
+            {constants.GROUP_INFO.GOAL} :{' '}
+            {groupInfo.partyGoal || constants.GROUP_INFO.NO_GOAL}
+          </div>
+          <div className='font-dnf text-2xl pl-5 pt-1'>
+            {constants.GROUP_INFO.COUNT} : {groupInfo.partyMembers?.length || 0}
+          </div>
+          {groupInfo.partyMembers?.length > 0 && (
+            <div className='grid grid-cols-2'>
+              {groupInfo.partyMembers.map((member) => (
+                <div key={member.memberId} className='flex pl-5 items-center'>
+                  <ProfileCat catId={member.catId} />
+                  <div className='font-neo text-xl pl-2'>
+                    {member.memberCatName}
+                  </div>
+                </div>
+              ))}
+            </div>
+          )}
+
+          <div className='flex flex-col font-dnf text-2xl pl-5 py-2'>
+            {constants.GROUP_INFO.POMODORO}
+          </div>
+          <MultiPomodoro groupId={partyId} />
+          <div className='flex justify-center pt-3'>
+            {myId === groupInfo.partyManagerId ? (
+              <Button
+                text={'그룹 설정'}
+                size='small'
+                color='navy'
+                onClick={() => navigate(`/groupSetting/${partyId}`)}
+              />
+            ) : (
+              <Button
+                text={'그룹 탈퇴'}
+                size='small'
+                color='gray'
+                onClick={() => clickLeaveGroup(partyId)}
+              />
+            )}
             <Button
-              text={'그룹 탈퇴'}
+              text={'뒤로 가기'}
               size='small'
               color='gray'
-              onClick={() => clickLeaveGroup(partyId)}
+              onClick={() => navigate(-1)}
             />
-          )}
-          <Button
-            text={'뒤로 가기'}
-            size='small'
-            color='gray'
-            onClick={() => navigate(-1)}
-          />
+          </div>
         </div>
+      </BasicFrame>
+      <div id='here' className='fixed right-0 bottom-0'>
+        {catIdList.map((cat, index) => (
+          <IdleCat
+            key={cat.catId}
+            catId={cat.catId}
+            position={{ right: 60 * index, bottom: 0 }} // 위치를 동적으로 설정
+          />
+        ))}
       </div>
-    </BasicFrame>
+    </>
   );
 };
 
